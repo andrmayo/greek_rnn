55fae3c810867a06e9d1f8fcf031eadab5190881
Andrew Mayo <acmayo399@gmail.com>
2026-02-10 17:09:22 -0500

    chore: log to stderr instead of stdout, with separate file logger for DEBUG

--- greek_rnn/main.py
+++ greek_rnn/main.py
@@ -37,20 +37,23 @@ class DecoderType(StrEnum):
 
 def setup_logging():
     logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
     formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
 
-    stdout_handler = logging.StreamHandler(sys.stdout)
-    stdout_handler.setLevel(logging.DEBUG)
-    stdout_handler.setFormatter(formatter)
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    stderr_handler.setLevel(logging.INFO)
+    stderr_handler.setFormatter(formatter)
 
-    file_handler = logging.FileHandler(
-        f"{Path(__file__).parent}/log/greek_data_processing.log"
-    )
+    file_handler = logging.FileHandler(f"{Path(__file__).parent}/log/greek_rnn.log")
     file_handler.setLevel(logging.INFO)
+    debug_file_handler = logging.FileHandler(
+        f"{Path(__file__).parent}/log/greek_rnn_debug.log"
+    )
+    debug_file_handler.setLevel(logging.DEBUG)
 
     logger.addHandler(file_handler)
-    logger.addHandler(stdout_handler)
+    logger.addHandler(debug_file_handler)
+    logger.addHandler(stderr_handler)
 
 
 model_name = "greek_rnn_no_dropout"
