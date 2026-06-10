1918d3ba00c2e9c10a26b9dadbd3286f763f7afc
andrmayo <acmayo399@gmail.com>
2026-06-10 13:12:16 -0400

    fix: typo, duplicate uvicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-exec /usr/src/app/.venv/bin/uvicorn uvicorn greek_rnn.api:app --host 0.0.0.0 --port ${PORT:-8080}
+exec /usr/src/app/.venv/bin/uvicorn greek_rnn.api:app --host 0.0.0.0 --port ${PORT:-8080}
