df285d41c780829bf70f96320faf4c3b8fbebd3d
andrmayo <acmayo399@gmail.com>
2026-06-10 12:56:19 -0400

    fix: call venv executable for uvicorn directory, avoiding uv run, which pulls in dependencies from pyproject.toml unnecessarily

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-exec uv run uvicorn greek_rnn.api:app --host 0.0.0.0 --port ${PORT:-8080}
+exec /usr/src/app/.venv/bin/uvicorn uvicorn greek_rnn.api:app --host 0.0.0.0 --port ${PORT:-8080}
